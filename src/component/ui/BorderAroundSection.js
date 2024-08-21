9e0441f3a75018ec178c10be67c08e0c221e8750
IbrahimMohamed9 <ibrahemmohamedb@gmail.com>
2024-08-21 09:34:37 +0200

    use the vriable

--- src/component/ui/BorderAroundSection.js
+++ src/component/ui/BorderAroundSection.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const BorderAroundSection = (props) => {
   return (
-    <div className={`min-h-fit p-5 w-full ${props.bg_color}`}>
-      <div className="container border-[#2D3648] border-5 h-full py-2 md:py-5 flex flex-col-reverse md:flex-row">
+    <div className={`min-h-fit p-5 w-full ${props.bg_color && true}`}>
+      <div className="container border-main-dark-blue border-5 h-full py-2 md:py-5 flex flex-col-reverse md:flex-row">
         {props.children}
       </div>
     </div>
