e914862a302c1c1c22b119e57a3eea21f249c752
IbrahimMohamed9 <ibrahemmohamedb@gmail.com>
2024-09-10 16:49:51 +0200

    just commit don't focus here

--- src/component/atoms/HeaderWordList.tsx
+++ src/component/atoms/HeaderWordList.tsx
@@ -12,7 +12,8 @@ const HeaderWordList = () => {
     "hover:text-main-green text-white w-20 transition-colors duration-300 cursor-pointer ";
 
   return (
-    <ul className="flex gap-30 ">
+    // TODO: delete hidden
+    <ul className="hidden md:flex gap-30 ">
       {content.map((word, index) => (
         <li key={index} className={wordClasses}>
           <Link
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+// import reportWebVitals from "./reportWebVitals";
 // import { Provider } from "react-redux";
 // import store from "./store";
 
@@ -15,4 +15,4 @@ root.render(
   </React.StrictMode>
 );
 
-reportWebVitals(console.log);
+// reportWebVitals(console.log);
