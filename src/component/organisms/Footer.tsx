1963bc10752cef6598dae18d563590024be9dcda
IbrahimMoatazMohamed <ibrahim.mohamed@stu.ibu.edu.ba>
2024-10-27 20:53:07 +0100

    make most famous articles in footer dynamic

--- src/component/organisms/Footer.tsx
+++ src/component/organisms/Footer.tsx
@@ -5,7 +5,27 @@ import { BsWhatsapp } from "react-icons/bs";
 import { LOGO_DESCRIPTION } from "../../assets/utils/Constants";
 import FooterWordList from "./../atoms/FooterWordList";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { fetchData } from "../../assets/customHooks/RestClient";
+
+type FooterWordListContent = [
+  {
+    content: string;
+    navTo: string;
+  }
+];
+
 const Footer = () => {
+  const [data, setData] = useState<FooterWordListContent>();
+  useEffect(() => {
+    fetchData("mostArticlesFooter", { setData });
+  }, []);
+
+  const mostFamousArticles = data && [
+    { content: "أشهر المقالات", navTo: "" },
+    ...data,
+  ];
+
   const whoWeAre = [
     { navTo: "", content: "من نحن؟" },
     { navTo: "/aboutUs", content: "اعرف عنا" },
@@ -13,22 +33,10 @@ const Footer = () => {
   ];
   const topics = [
     { navTo: "", content: "المواضيع" },
-    { navTo: "/DoctrinalEducation", content: "التربية العقائدية" },
-    { navTo: "/FamilyAndLife", content: "الاسرة و الحياة" },
-    { navTo: "/Raising", content: "التربية" },
-    { navTo: "/Health", content: "الصحة" },
-  ];
-  const mostPopularArticles = [
-    { navTo: "", content: "اشهر المقالات" },
-    { navTo: "/thinkingEducation", content: "التربية بالتفكر" },
-    {
-      navTo: "/islamicEducationLibrary",
-      content: "مكتبة التربية الإسلامية",
-    },
-    {
-      navTo: "/sexualEducationInIslam",
-      content: "التربية الجنسية في الإسلام",
-    },
+    { navTo: "/articles/DoctrinalEducation", content: "التربية العقائدية" },
+    { navTo: "/articles/FamilyAndLife", content: "الاسرة و الحياة" },
+    { navTo: "/articles/Raising", content: "التربية" },
+    { navTo: "/articles/Health", content: "الصحة" },
   ];
   const commonSvgClasses =
     "size-8 border-2 p-1 rounded-full transition-all duration-300";
@@ -79,10 +87,12 @@ const Footer = () => {
           </Link>
           <ul className="flex gap-10">{socialMediaIconsElements}</ul>
         </div>
-        <FooterWordList
-          className="max-xs:text-center"
-          content={mostPopularArticles}
-        />
+        {mostFamousArticles && (
+          <FooterWordList
+            className="max-xs:text-center"
+            content={mostFamousArticles}
+          />
+        )}
         <FooterWordList className="max-sm:hidden" content={topics} />
         <FooterWordList className="max-xs:text-center" content={whoWeAre} />
       </div>
